9cf32d473e3fddc401183af4bf263800e7adccd0
Hedda Olimb <72741430+heddaoli@users.noreply.github.com>
2022-10-13 20:40:23 +0200

    done

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 basic.forever(function () {
-	
+    if (smarthome.ReadSoilHumidity(AnalogPin.P1) < 50) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.showIcon(IconNames.Sad)
+        basic.pause(2000)
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.showIcon(IconNames.Happy)
+        basic.pause(1000)
+    }
 })
